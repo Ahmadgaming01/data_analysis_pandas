3a34fd315af5377097333d41ea14b425f6ddb51e
ahmad <aalmadani111@gmail.com>
2024-03-16 03:04:48 +0100

    Begin learn data analysis with pandas

---
+++ PandaIntro.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+# data frame use list -------->>
+
+# data = [1,2,3,4,5]
+# df = pd.DataFrame(data , columns = ['Number'])
+# print(df)
+
+
+# data frame use dic.list ------------>>
+
+# data = [['ahmad', 30] , ['ali',25] , ['rami',35]]
+# df = pd.DataFrame(data , columns=['name' , 'age'] , index=['person one ' , 'person tow' , 'person three'])
+
+# print(df)
+
+
+
+
+
